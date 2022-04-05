6665a78339b5abd2692b1a6b0948886a7907747f
LorraineFG <lorrainefreitas.amb@gmail.com>
2022-04-05 01:05:59 -0300

    terminado o edit task e o create new task

--- Documents/Python/TaskList/todo/task/views.py
+++ Documents/Python/TaskList/todo/task/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
 
 
 def taskList(request):
-  tasks = Task.objects.all()
+  tasks = Task.objects.all().order_by('-created_al')
   return render(request, 'task/list.html', {'tasks': tasks})
 
 def taskView(request, id):
@@ -13,8 +13,35 @@ def taskView(request, id):
   return render(request, 'task/task.html', {'task': task})
 
 def newTask(request):
-  form = TaskForm()
+  if request.method == 'POST':
+    form = TaskForm(request.POST)
+
+    if form.is_valid():
+      task = form.save(commit=False)
+      task.done = 'doing'
+      task.save()
+      return redirect('/')
+
+  else:
+    form = TaskForm()
   return render(request, 'task/addtask.html', {'form':form})
+
+def editTask(request, id):
+  task = get_object_or_404(Task, pk=id)
+  form = TaskForm(instance=task)
+
+  if(request.method == 'POST'):
+    
+    form = TaskForm(request.POST, instance=task)
+   
+    if(form.is_valid()):
+      task.save()
+      return redirect ('/')
+    else:
+      return render(request, 'task/edittask.html', {'form':form, 'task':task})
+
+  else:
+    return render(request, 'task/edittask.html', {'form':form, 'task':task})
 
 def helloWorld(request):
   return HttpResponse(" Hello World! ")
